e8bf167eefea3b7b80c6af29498a53fb5499d66b
takticode <sagiv0611@gmail.com>
2025-11-09 16:26:11 +0200

    javac ascend sagiv cohen

--- Ascend.java
+++ Ascend.java
@@ -2,6 +2,26 @@
 // and prints them in increasing order.
 public class Ascend {
 	public static void main(String[] args) {
-		// Replace this comment with your code
+	 int min = 1;
+	 int max = 100;
+	 int a = (int)(Math.random() * (max - min + 1)) + min;
+	 int b = (int)(Math.random() * (max - min + 1)) + min;
+	 int c = (int)(Math.random() * (max - min + 1)) + min;
+
+	 if ( a > b ) {
+     int temp = a; a = b; b = temp;
+	 }
+
+     if ( b > c ) {  
+	 int temp = b; b = c; c = temp;
+	}	
+
+     if ( a > b ) {
+	 int temp = a; a = b; b = temp;
+	 }
+
+     System.out.println( a + " " + b + " " + c);
+
 	}
 }
+	
