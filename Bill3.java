378a30305bb3cb115345af3985ff54adb740d70e
takticode <sagiv0611@gmail.com>
2025-11-09 16:42:42 +0200

    Bill3.java sagiv cohen

--- Bill3.java
+++ Bill3.java
@@ -3,6 +3,21 @@ public class Bill3 {
 	public static void main(String[] args) {
 		// To get you started, here is the first line in the program:
 	    String name1 = args[0];
-	    // Replace this comment with the rest of your code   
+		String name2 = args[1];
+		String name3 = args[2];
+
+		double totalBill = Double.parseDouble(args[3]);
+
+		double share = totalBill / 3.0;
+
+		System.out.println(name1 + " pays " + share);
+		System.out.println(name2 + " pays " + share);
+		System.out.println(name3 + " pays " + share);
+		
 	}
 }
+
+
+	      
+	
+
