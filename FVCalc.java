eb4b34a172f51beb2798e5a8799f3b0931dfec27
takticode <sagiv0611@gmail.com>
2025-11-09 16:53:59 +0200

    FVCalc.java sagiv cohen

--- FVCalc.java
+++ FVCalc.java
@@ -1,6 +1,13 @@
 // Computes the future value of a saving investment.
 public class FVCalc {
 	public static void main(String[] args){
-		// Replace this comment with your code
+		double principal = Double.parseDouble(args[0]);
+		double rate = Double.parseDouble(args[1]);
+		int years = Integer.parseInt(args[2]);
+
+		double futureValue = principal * Math.pow(1 + rate / 100.0, years);
+		System.out.println("Future value after " + years + " years is: " + futureValue); 
+
 	}
+	
 }
